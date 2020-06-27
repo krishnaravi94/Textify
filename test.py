a110bec5028f2728561695f5ff1f5ad4b05987af
krishnaravi94 <45928059+krishnaravi94@users.noreply.github.com>
2020-06-27 18:01:18 +0530

    commit #2

--- test.py
+++ test.py
@@ -1,22 +1,31 @@
-from tkinter import Tk, Label, Button
 
-class MyFirstGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("A simple GUI")
+# from tkinter import Tk, Label, Button
 
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
-
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
-
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
-
-    def greet(self):
-        print("Greetings!")
-
-root = Tk()
-my_gui = MyFirstGUI(root)
-root.mainloop()
+# class MyFirstGUI:
+#     def __init__(self, master):
+#         self.master = master
+#         master.title("A simple GUI")
+#
+#         self.label = Label(master, text="This is our first GUI!")
+#         self.label.pack()
+#
+#         self.greet_button = Button(master, text="Greet", command=self.greet)
+#         self.greet_button.pack()
+#
+#         self.close_button = Button(master, text="Close", command=master.quit)
+#         self.close_button.pack()
+#
+#     def greet(self):
+#         print("Greetings!")
+#
+# root = Tk()
+# my_gui = MyFirstGUI(root)
+# root.mainloop()
+list1 = [1,2,4,6,7,10,11,13,15,16]
+celsiusToFarenheitList = list(map(lambda x: float((9/5)*x) +32,list1))
+print(celsiusToFarenheitList)
+# evenList = list(filter(lambda result: result%2 == 0,list1))
+# print(evenList)
+# evenSquaredList = map(lambda squaredNumber: squaredNumber ** 2,evenList)
+# print(evenSquaredList)
+# evenSquaredList = map(result())
